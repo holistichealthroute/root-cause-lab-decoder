49f0fd96e995edf1915fa0875b5c03aa7f6b70c9
holistichealthroute <livingwithelevation@gmail.com>
2025-11-01 00:29:41 +0500

    fix log file name

--- backend/app/main.py
+++ backend/app/main.py
@@ -36,7 +36,7 @@ app.state.oauth = oauth
 
 app.add_middleware(
     CORSMiddleware,
-   allow_origins=["*"],         
+   allow_origins=["https://root-cause-lab-decoder-production.up.railway.app"],         
     allow_methods=["*"],            
     allow_headers=["*"],            
 )
